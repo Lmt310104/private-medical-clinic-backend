40520e4f5e205c57f2eb9567f8686a83a9c95c3e
Lmt310104 <lmtoan311@gmail.com>
2024-05-15 09:34:05 +0700

    feat: save drug at the last day of the month

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,7 +5,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
-  null,
+  process.env.DB_PASSWORD,
   {
     host: "localhost",
     dialect: "mysql",
