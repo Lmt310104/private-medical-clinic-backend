c617837f3844ef9e924aedbdfb72609da41c65af
Lmt310104 <lmtoan311@gmail.com>
2024-05-21 11:38:50 +0700

    Merge branch 'booking' of https://github.com/Lmt310104/private-medical-clinic-backend into dev

---
+++ src/migrations/20240516101054-create-booking-appointment-list.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('bookingAppointmentLists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      bookingDate: {
+        type: Sequelize.DATE
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      bookingAppointment: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('bookingAppointmentLists');
+  }
+};
